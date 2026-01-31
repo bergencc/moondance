a9d3961d377ef3091639ae05652aed54b3a15873
ghost <belikesnab@gmail.com>
2026-01-31 13:33:51 -0500

    init

---
+++ backend/src/main/java/org/bosf/moondance/MoondanceApplication.java
@@ -0,0 +1,13 @@
+package org.bosf.moondance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoondanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoondanceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/bosf/moondance/MoondanceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bosf.moondance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoondanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
